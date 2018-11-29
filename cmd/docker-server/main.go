47fe9cd78a9e5ad5be013c2ac35b660922638659
Grant Griffiths <ggp493@gmail.com>
2018-11-28 18:01:47 -0800

    initial work on main.go
    Signed-off-by: Grant Griffiths <ggp493@gmail.com>

--- cmd/docker-server/main.go
+++ cmd/docker-server/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/libopenstorage/openstorage/volume"
+	"github.com/lpabon/openstorage-docker-server/pkg/server"
+)
 
 func main() {
+	server.StartGraphAPI("fake", volume.PluginAPIBase)
+
 	fmt.Println("vim-go")
 }
